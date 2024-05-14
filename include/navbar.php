f4343e6e22c42a65cd943712b5f573b3ced858dd
beneboyyyy <bengalgo04@gmail.com>
2024-05-14 08:17:46 +0800

    changes in navbar

--- include/navbar.php
+++ include/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
   <a class="navbar-brand" href="#">Welcome, <?php echo $_SESSION['user_name'];?>! </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
